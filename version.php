e7b7ed760313145c777e36b0e9fc1ef7a12f96de
jfedericobn <jesus@blindsidenetworks.com>
2026-09-04 15:20:00 -0400

    chore: align support range for marketplace

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->component    = 'bbbext_bnx';
 $plugin->release      = '1.2';
 $plugin->version      = 2026090400;
 $plugin->requires     = 2025100600; // Moodle 5.1.0 minimum.
-$plugin->supported    = [501, 503];
+$plugin->supported    = [501, 502];
 $plugin->maturity     = MATURITY_STABLE;
